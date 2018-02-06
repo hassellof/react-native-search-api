8e7b447249e955c0fc938ade4854fa049e0baf4f
Daniil Konoplev <danchoys@icloud.com>
2018-02-06 17:16:13 +0300

    Implement the image loading

--- ios/RCTSearchApi/RCTSearchApiManager.m
+++ ios/RCTSearchApi/RCTSearchApiManager.m
@@ -12,11 +12,14 @@
 #import "RCTSearchApiManager.h"
 #import "NSDictionary+RCTSearchApi.h"
 #import <React/RCTUtils.h>
+#import <React/RCTImageLoader.h>
 
 static NSString *const kHandleContinueUserActivityNotification = @"handleContinueUserActivity";
 static NSString *const kUserActivityKey = @"userActivity";
 static NSString *const kSpotlightSearchItemTapped = @"spotlightSearchItemTapped";
 static NSString *const kAppHistorySearchItemTapped = @"appHistorySearchItemTapped";
+
+typedef void (^ContentAttributeSetCreationCompletion)(CSSearchableItemAttributeSet *set, NSError *error);
 
 @interface RCTSearchApiManager ()
 
@@ -59,8 +62,7 @@ RCT_EXPORT_MODULE();
 
 #pragma mark - Properties
 
-- (dispatch_queue_t)methodQueue
-{
+- (dispatch_queue_t)methodQueue {
     return dispatch_get_main_queue();
 }
 
@@ -71,11 +73,9 @@ RCT_EXPORT_MODULE();
 + (NSMutableArray *)activityQueue {
     static NSMutableArray *activityQueue;
     static dispatch_once_t onceToken;
-    
     dispatch_once(&onceToken, ^{
         activityQueue = [NSMutableArray array];
     });
-    
     return activityQueue;
 }
 
@@ -94,19 +94,31 @@ RCT_EXPORT_MODULE();
 
 #pragma mark - Exported API
 
-RCT_EXPORT_METHOD(indexItem:(NSDictionary *)item resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject) {
-    return [self indexItems:@[item] resolve:resolve reject:reject];
-}
-
 RCT_EXPORT_METHOD(indexItems:(NSArray *)items resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject) {
+    if (items.count == 0)
+        return resolve(nil);
+    dispatch_group_t group = dispatch_group_create();
     NSMutableArray *itemsToIndex = [NSMutableArray array];
     [items enumerateObjectsUsingBlock:^(NSDictionary *item, NSUInteger idx, BOOL * _Nonnull stop) {
-        CSSearchableItem *searchableItem = [[CSSearchableItem alloc] initWithUniqueIdentifier:item.rctsa_uniqueIdentifier
-                                                                             domainIdentifier:item.rctsa_domain
-                                                                                 attributeSet:[self contentAttributeSetFromItem:item]];
-        [itemsToIndex addObject:searchableItem];
+        dispatch_group_enter(group);
+        [self createContentAttributeSetFromItem:item withCompletion:^(CSSearchableItemAttributeSet *set, NSError *error) {
+            if (set && !error) {
+                CSSearchableItem *searchableItem = [[CSSearchableItem alloc] initWithUniqueIdentifier:item.rctsa_uniqueIdentifier
+                                                                                     domainIdentifier:item.rctsa_domain
+                                                                                         attributeSet:set];
+                [itemsToIndex addObject:searchableItem];
+            }
+            dispatch_group_leave(group);
+        }];
     }];
-    [[CSSearchableIndex defaultSearchableIndex] indexSearchableItems:itemsToIndex completionHandler:[self completionBlockWithResolve:resolve reject:reject]];  
+    dispatch_group_notify(group, self.methodQueue, ^{
+        if (itemsToIndex.count == items.count) {
+            return [[CSSearchableIndex defaultSearchableIndex] indexSearchableItems:itemsToIndex completionHandler:[self completionBlockWithResolve:resolve reject:reject]];
+        } else {
+            NSError *e = RCTErrorWithMessage(@"Failed to create one or more content attribute sets");
+            reject(RCTErrorUnspecified, e.localizedDescription, e);
+        }
+    });
 }
 
 RCT_EXPORT_METHOD(deleteItemsWithIdentifiers:(NSArray *)identifiers resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject) {
@@ -122,29 +134,36 @@ RCT_REMAP_METHOD(deleteAllItems, resolve:(RCTPromiseResolveBlock)resolve reject:
 }
 
 RCT_EXPORT_METHOD(createUserActivity:(NSDictionary *)item resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject) {
-    NSUserActivity *userActivity = [[NSUserActivity alloc] initWithActivityType:[NSBundle mainBundle].bundleIdentifier];
-    userActivity.title = item.rctsa_title;
-    userActivity.userInfo = item.rctsa_userInfo;
-    userActivity.eligibleForPublicIndexing = item.rctsa_eligibleForPublicIndexing;
-    userActivity.expirationDate = item.rctsa_expirationDate;
-    userActivity.webpageURL = item.rctsa_webpageURL;
-    userActivity.contentAttributeSet = [self contentAttributeSetFromItem:item];
-    userActivity.eligibleForSearch = YES;
-    userActivity.eligibleForHandoff = NO;
-    [userActivity becomeCurrent];
-    [self.userActivities addObject:userActivity];
-    resolve(nil);
+    [self createContentAttributeSetFromItem:item withCompletion:^(CSSearchableItemAttributeSet *set, NSError *error) {
+        dispatch_async(self.methodQueue, ^{
+            if (error || !set) {
+                NSError *e = error ?: RCTErrorWithMessage(@"Could not create a content attribute set");
+                reject(RCTErrorUnspecified, e.localizedDescription, e);
+            } else {
+                NSUserActivity *userActivity = [[NSUserActivity alloc] initWithActivityType:[NSBundle mainBundle].bundleIdentifier];
+                userActivity.contentAttributeSet = set;
+                userActivity.title = item.rctsa_title;
+                userActivity.userInfo = item.rctsa_userInfo;
+                userActivity.eligibleForPublicIndexing = item.rctsa_eligibleForPublicIndexing;
+                userActivity.expirationDate = item.rctsa_expirationDate;
+                userActivity.webpageURL = item.rctsa_webpageURL;
+                userActivity.eligibleForSearch = YES;
+                userActivity.eligibleForHandoff = NO;
+                [userActivity becomeCurrent];
+                [self.userActivities addObject:userActivity];
+                resolve(nil);
+            }
+        });
+    }];
 }
 
 #pragma mark - Private API
 
 - (void)drainActivityQueue {
     NSMutableArray *activityQueue = [[self class] activityQueue];
-    
     for (NSUserActivity *userActivity in activityQueue) {
         [self handleContinueUserActivity:userActivity];
     }
-    
     [activityQueue removeAllObjects];
 }
 
@@ -157,7 +176,6 @@ RCT_EXPORT_METHOD(createUserActivity:(NSDictionary *)item resolve:(RCTPromiseRes
     } else {
         [self sendEventWithName:kAppHistorySearchItemTapped body:userActivity.userInfo];
     }
-    
 }
 
 - (void (^)(NSError * _Nullable error))completionBlockWithResolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject {
@@ -170,13 +188,33 @@ RCT_EXPORT_METHOD(createUserActivity:(NSDictionary *)item resolve:(RCTPromiseRes
     };
 }
 
-- (CSSearchableItemAttributeSet *)contentAttributeSetFromItem:(NSDictionary *)item {
+- (void)createContentAttributeSetFromItem:(NSDictionary *)item withCompletion:(ContentAttributeSetCreationCompletion)completionBlock {
     CSSearchableItemAttributeSet *attributeSet = [[CSSearchableItemAttributeSet alloc] initWithItemContentType:(NSString *)kUTTypeJSON];
     attributeSet.title = item.rctsa_title;
     attributeSet.contentDescription = item.rctsa_contentDescription;
     attributeSet.keywords = item.rctsa_keywords;
-    attributeSet.thumbnailURL = item.rctsa_thumbnailURL;
-    return attributeSet;
+    if (item.rctsa_thumbnail) {
+        [self loadImageFromSource:item.rctsa_thumbnail withCompletion:^(NSError *error, UIImage *image) {
+            if (error || !image) {
+                return completionBlock(nil, error ?: RCTErrorWithMessage(@"Could not load an image"));
+            }
+            attributeSet.thumbnailData = UIImagePNGRepresentation(image);
+            completionBlock(attributeSet, nil);
+        }];
+    } else {
+        completionBlock(attributeSet, nil);
+    }
+}
+
+- (void)loadImageFromSource:(RCTImageSource *)source withCompletion:(RCTImageLoaderCompletionBlock)completionBlock {
+    [self.bridge.imageLoader loadImageWithURLRequest:source.request
+                                                size:source.size
+                                               scale:source.scale
+                                             clipped:YES
+                                          resizeMode:RCTResizeModeStretch
+                                       progressBlock:NULL
+                                    partialLoadBlock:NULL
+                                     completionBlock:completionBlock];
 }
 
 @end
